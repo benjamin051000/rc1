9805797d15d2fc5ba2e88aec7f095fa10a994e7a
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-08-29 16:29:04 -0400

    synthesized some more stuff

--- lab0/lab0.runs/synth_1/mult_pipe.tcl
+++ lab0/lab0.runs/synth_1/mult_pipe.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "D:/Repos/reconfig/lab0/lab0.runs/synth_1/enc4to2_tb.tcl"
+  variable script "D:/Repos/reconfig/lab0/lab0.runs/synth_1/mult_pipe.tcl"
   variable category "vivado_synth"
 }
 
@@ -87,10 +87,7 @@ set_property ip_output_repo d:/Repos/reconfig/lab0/lab0.cache/ip [current_projec
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_vhdl -library xil_defaultlib {
-  D:/Repos/reconfig/lab0/src/enc4to2/enc4to2.vhd
-  D:/Repos/reconfig/lab0/src/enc4to2/enc4to2_tb.vhd
-}
+read_vhdl -library xil_defaultlib D:/Repos/reconfig/lab0/src/mult_pipe/mult_pipe.vhd
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -104,7 +101,7 @@ set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
-synth_design -top enc4to2_tb -part xc7k70tfbv676-1
+synth_design -top mult_pipe -part xc7k70tfbv676-1
 OPTRACE "synth_design" END { }
 if { [get_msg_config -count -severity {CRITICAL WARNING}] > 0 } {
  send_msg_id runtcl-6 info "Synthesis results are not added to the cache due to CRITICAL_WARNING"
@@ -114,10 +111,10 @@ if { [get_msg_config -count -severity {CRITICAL WARNING}] > 0 } {
 OPTRACE "write_checkpoint" START { CHECKPOINT }
 # disable binary constraint mode for synth run checkpoints
 set_param constraints.enableBinaryConstraints false
-write_checkpoint -force -noxdef enc4to2_tb.dcp
+write_checkpoint -force -noxdef mult_pipe.dcp
 OPTRACE "write_checkpoint" END { }
 OPTRACE "synth reports" START { REPORT }
-create_report "synth_1_synth_report_utilization_0" "report_utilization -file enc4to2_tb_utilization_synth.rpt -pb enc4to2_tb_utilization_synth.pb"
+create_report "synth_1_synth_report_utilization_0" "report_utilization -file mult_pipe_utilization_synth.rpt -pb mult_pipe_utilization_synth.pb"
 OPTRACE "synth reports" END { }
 file delete __synthesis_is_running__
 close [open __synthesis_is_complete__ w]
